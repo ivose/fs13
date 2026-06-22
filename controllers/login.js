a85c7ed76594d8308e80c860bf2035e308f9c730
ivose <ivoseeba@gmail.com>
2026-06-22 12:17:54 +0300

    e23 - Better bookkeeping

--- controllers/login.js
+++ controllers/login.js
@@ -4,39 +4,49 @@ const router = require('express').Router()
 const { SECRET } = require('../util/config')
 const User = require('../models/user')
 
-router.post('/', async (request, response) => {
-  const body = request.body
+router.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
 
-  const user = await User.findOne({
-    where: {
-      username: body.username
+    const user = await User.findOne({
+      where: {
+        username: body.username
+      }
+    })
+
+    const passwordCorrect = user && body.password === user.password
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'invalid username or password'
+      })
     }
-  })
 
-  const passwordCorrect = user && body.password === user.password
+    if (user.disabled) {
+      return response.status(401).json({
+        error: 'account disabled, please contact admin'
+      })
+    }
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
-    })
+    if (!SECRET) {
+      return response.status(500).json({
+        error: 'SECRET is missing from environment variables'
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    }
+
+    const token = jwt.sign(userForToken, SECRET)
+
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name })
+  } catch (error) {
+    next(error)
   }
-
-  if (user.disabled) {
-    return response.status(401).json({
-      error: 'account disabled, please contact admin'
-    })
-  }
-
-  const userForToken = {
-    username: user.username,
-    id: user.id,
-  }
-
-  const token = jwt.sign(userForToken, SECRET)
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = router
--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const { Blog, User, ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 const formatReadingList = (entry) => ({
   id: entry.id,
@@ -66,5 +67,65 @@ router.post('/', async (req, res, next) => {
     next(error)
   }
 })
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const readingListEntry = await ReadingList.findByPk(req.params.id)
+
+    if (!readingListEntry) {
+      return res.status(404).json({
+        error: 'reading list entry not found'
+      })
+    }
+
+    if (readingListEntry.userId !== req.decodedToken.id) {
+      return res.status(401).json({
+        error: 'operation not allowed'
+      })
+    }
+
+    if (typeof req.body.read !== 'boolean') {
+      return res.status(400).json({
+        error: 'read must be a boolean'
+      })
+    }
+
+    readingListEntry.read = req.body.read
+    await readingListEntry.save()
+
+    res.json(formatReadingList(readingListEntry))
+  } catch (error) {
+    next(error)
+  }
+})
+/*
+for example in power shell:
+
+$loginResponse = Invoke-RestMethod `
+  -Uri http://localhost:3001/api/login `
+  -Method Post `
+  -ContentType "application/json" `
+  -Body '{"username":"sample.admin@example.com","password":"password123"}'
+
+$token = $loginResponse.token
+$token
+
+
+$user = Invoke-RestMethod `
+  -Uri http://localhost:3001/api/users/4 `
+  -Method Get
+
+$user | ConvertTo-Json -Depth 10
+
+
+$readingListId = $user.readings[0].reading_list.id
+
+Invoke-RestMethod `
+  -Uri "http://localhost:3001/api/readinglists/$readingListId" `
+  -Method Put `
+  -Headers @{ Authorization = "Bearer $token" } `
+  -ContentType "application/json" `
+  -Body '{"read":true}'
+*/
 
 module.exports = router
